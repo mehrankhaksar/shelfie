858cccea3a21a134a07740bac0cf37386b7a625b
mehrankhaksar <mehran9326@gmail.com>
2026-04-09 23:59:10 +0330

    add real-time data

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,9 +1,11 @@
 import { createContext, useEffect, useState } from "react";
-import { databases } from "../lib/appWrite";
+import { client, databases } from "../lib/appWrite";
 import { ID, Permission, Query, Role } from "react-native-appwrite";
 import { useUser } from "../hooks/useUser";
 
 export const BooksContext = createContext();
+const DB_ID = process.env.EXPO_PUBLIC_DB_ID;
+const BOOK_COLLECTION_ID = process.env.EXPO_PUBLIC_BOOK_COLLECTION_ID;
 
 export default function BooksProvider({ children }) {
   const [books, setBooks] = useState([]);
@@ -15,8 +17,8 @@ export default function BooksProvider({ children }) {
     setIsLoading(true);
     try {
       const response = await databases.listDocuments({
-        databaseId: process.env.EXPO_PUBLIC_DB_ID,
-        collectionId: process.env.EXPO_PUBLIC_BOOK_COLLECTION_ID,
+        databaseId: DB_ID,
+        collectionId: BOOK_COLLECTION_ID,
         queries: [Query.equal("userId", user.$id)],
       });
       setBooks(response.documents);
@@ -30,8 +32,8 @@ export default function BooksProvider({ children }) {
     setIsLoading(true);
     try {
       await databases.createDocument({
-        databaseId: process.env.EXPO_PUBLIC_DB_ID,
-        collectionId: process.env.EXPO_PUBLIC_BOOK_COLLECTION_ID,
+        databaseId: DB_ID,
+        collectionId: BOOK_COLLECTION_ID,
         documentId: ID.unique(),
         data: { ...data, userId: user.$id },
         permissions: [
@@ -48,11 +50,22 @@ export default function BooksProvider({ children }) {
   };
 
   useEffect(() => {
+    let unsubscribe;
+    const channel = `databases.${DB_ID}.collections.${BOOK_COLLECTION_ID}.documents`;
     if (user) {
       getBooks();
+      unsubscribe = client.subscribe(channel, ({ payload, events }) => {
+        if (events[0].includes("create")) {
+          setBooks((prev) => [...prev, payload]);
+        }
+      });
     } else {
       setBooks([]);
     }
+
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
   }, [user]);
 
   return (
